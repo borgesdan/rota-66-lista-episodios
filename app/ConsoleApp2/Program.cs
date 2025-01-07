076752690558a1b89bc27130777bdcce965025ca
Danilo Borges Santos <borges_santos89@hotmail.com>
2025-01-07 14:24:35 -0300

    Adiciona projeto para consulta em api

---
+++ app/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp2;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+var rotaApi = new RotaApi();
+using var client = new HttpClient();
+var response = new ClipResponse();
+
+while (true)
+{
+    var url = rotaApi.ToString();
+    var result = await client.GetAsync(url);
+    var clip = await result.Content.ReadFromJsonAsync<ClipResponse>();
+
+    if (clip == null || !clip.Clips.Any())
+        break;
+
+    var last = clip.Clips.Last();
+    var responseLast = response.Clips.LastOrDefault();
+
+    if (responseLast != null && responseLast.Id == last.Id)
+        break;
+
+    response.Clips.AddRange(clip.Clips);
+    rotaApi.ClipId = last.Id.ToString();
+
+    await Task.Delay(2000);
+}
+
+var json = JsonSerializer.Serialize(response.Clips);
+File.AppendAllText("c:/rota66.json", json);
+
+var clipsOrdened = response.Clips.DistinctBy(c => c.Id).OrderBy(c => c.Episode).ToList();
+
+StringBuilder sb = new StringBuilder();
+
+foreach (var cl in clipsOrdened)
+{
+    sb.AppendLine($"- {cl.Episode} - {cl.Title}");
+}
+
+File.AppendAllText("c:/rota66Ordened.txt", sb.ToString());
---
+++ app/ConsoleApp2/RotaResponse.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class RotaApi
+    {
+        public string Url { get; set; } = "https://api.omny.fm/programs/rota-66/playlists/podcast/clips";
+        public string PageSize { get; set; } = "50";
+        public string IncludeProgramDetail { get; set; } = "false";
+        public string ClipId { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            var param = new Dictionary<string, string>() 
+            { 
+                { "pageSize", PageSize },
+                { "icludeProgramDetail", IncludeProgramDetail },
+                { "clipId", ClipId },
+            };
+
+            var newUrl = new Uri(QueryHelpers.AddQueryString(Url, param));
+            return newUrl.ToString();
+        }
+    }
+
+    internal class RotaResponse
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Season { get; set; }
+        public int Episode { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Id: ");
+            sb.AppendLine(Id.ToString());
+            sb.Append("Title: ");
+            sb.AppendLine(Title);
+            sb.Append("Season: ");
+            sb.AppendLine(Season.ToString());
+            sb.Append("Episode: ");
+            sb.AppendLine(Episode.ToString());
+
+            return sb.ToString();
+        }
+    }
+
+    internal class ClipResponse
+    {
+        public List<RotaResponse> Clips { get; set; } = [];
+    }
+}
